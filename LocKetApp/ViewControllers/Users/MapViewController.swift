f68e816c786dd0b097c4177d9d8957fe49dbbe1e
NA-anna <laanna1001@gmail.com>
2022-11-28 11:56:21 +0900

    When map marker clicked, present detail view

--- LocKetApp/ViewControllers/Users/MapViewController.swift
+++ LocKetApp/ViewControllers/Users/MapViewController.swift
@@ -139,17 +139,11 @@ class MapViewController: UIViewController, MTMapViewDelegate {
     
 
     func mapView(_ mapView: MTMapView!, touchedCalloutBalloonOf poiItem: MTMapPOIItem!) {
-        print("선택!")
         
-//        // 스토리보드의 파일 찾기
-//        let storyboard: UIStoryboard? = UIStoryboard(name: "FleaMarketDetailSt", bundle: Bundle.main)
-//        // 스토리보드에서 지정해준 ViewController의 ID
-//        guard let vc = storyboard?.instantiateViewController(identifier: "marketStoryboard") else { return }
-//        // 화면 전환방식 선택 (default : .modal)
-//        //vc.modalTransitionStyle = .fullScreen
-//        // 화면 전환!
-//        self.present(vc, animated: true)
-        
+        let storyBoard = UIStoryboard(name: "FleaMarketDetailSt", bundle: nil)
+        let vc = storyBoard.instantiateViewController(withIdentifier: "FleaMarketDetail")
+        self.navigationController?.pushViewController(vc, animated: true)
+    
     }
     
     
